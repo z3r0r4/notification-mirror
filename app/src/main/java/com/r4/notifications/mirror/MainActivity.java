aedb4b91863a5886094bb5b1c395480ed15ca65a
unknown <R4C1.meru@protonmail.com>
2020-10-23 00:10:28 +0200

    empty Activity

---
+++ app/src/main/java/com/r4/notifications/mirror/MainActivity.java
@@ -0,0 +1,14 @@
+package com.r4.notifications.mirror;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
