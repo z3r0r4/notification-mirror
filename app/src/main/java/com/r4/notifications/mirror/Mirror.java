85ce24de6c32a46a26cffb3f4315886e55a0c6f5
unknown <R4C1.meru@protonmail.com>
2020-10-29 17:15:44 +0100

    working tcp socket connection

--- app/src/main/java/com/r4/notifications/mirror/Mirror.java
+++ app/src/main/java/com/r4/notifications/mirror/Mirror.java
@@ -6,7 +6,10 @@ import android.util.Log;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
 import java.net.Socket;
+import java.net.SocketAddress;
 
 class Mirror extends AsyncTask<String, Void, Void> {
     private final static String TAG = "Mirror";
@@ -22,14 +25,19 @@ class Mirror extends AsyncTask<String, Void, Void> {
         try {
 //            mSocket.checkConnect();
             Log.d(TAG, "doInBackground: sending");
-            mSocket = new Socket("192.168.178.10", 9999);
-            mPrintWriter = new PrintWriter(mSocket.getOutputStream(),true);
+            mSocket = new Socket();
+            InetAddress inetAddress = InetAddress.getByName("192.168.178.10");
+            SocketAddress socketAddress = new InetSocketAddress(inetAddress, 9999);
+//            mSocket.bind(socketAddress);
+            mSocket.connect(socketAddress, 5000);
+            mPrintWriter = new PrintWriter(mSocket.getOutputStream(), true);
             mPrintWriter.println("This is a message sent to the server");
             //            mPrintWriter.write(message);
 //            mPrintWriter.flush();
             mPrintWriter.close();
             mSocket.close();
             Log.d(TAG, "doInBackground: SENNT");
+
         } catch (IOException e) {
             Log.e(TAG, "doInBackground: COULDNT CONNCET", e);
         }
